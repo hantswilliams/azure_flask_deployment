7b53a1a6fbc69e6836434ae1cf36f9b41a21f25d
hantswilliams <hantsawilliams@gmail.com>
2023-09-12 00:02:36 +0000

    small change

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import pandas as pd
+
+# small change
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8080
+    )
